87043f610feb53b6d0fb9db5beadc358e52cb513
tellefk <tellef94@hotmail.com>
2022-11-03 21:54:10 +0100

    store in app

--- src/store/todos-context.tsx
+++ src/store/todos-context.tsx
@@ -1,19 +1,52 @@
-import React, {useState} from "react"
-import Todo from "../models/todo"
+import React, { useState } from "react";
+import Todo from "../models/todo";
 
-const TodoContext=React.createContext<{
-    items:Todo[],
-    addTodo:()=>void,
-    removeTodo:()=>void
-}>({
-    items:[],
-    addTodo:()=>{},
-    removeTodo:()=>(id:string)=>{}
-})
+type TodosContext = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+type Props = {
+    children: React.ReactNode
+ }
+
+export const TodoContext = React.createContext<TodosContext>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
 
 
-const TodosContextProvider:React.FC=(props)=>{
-    
 
-    return <TodoContext.Provider value={TodoContext} >{props.children}</TodoContext.Provider>
-}
+const TodosContextProvider: React.FC<Props> = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function addTodo(enteredText: string) {
+    const newTodo = new Todo(enteredText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  }
+
+  function removeTodo(id: string) {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  const contextValue: TodosContext = {
+    items: todos,
+    addTodo: addTodo,
+    removeTodo: removeTodo,
+  };
+
+  return (
+    <TodoContext.Provider value={contextValue}>
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
+
+
+export default TodosContextProvider
